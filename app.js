253798b417867048bb67bdc954a841369f770e72
Waliston Belles <walistonbelles@bol.com.br>
2021-04-26 16:16:41 -0300

    Adicionado classe para processar os dados lidos

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var Reader = require('./Reader');
+var Processor = require('./Processor');
 
 var leitor = new Reader();
 
 async function main(){
     var dados = await leitor.Read('./users.csv');
-    console.log(dados);
-} 
+    var dadosProcessados = Processor.Process(dados);
+}   
 
 main();
