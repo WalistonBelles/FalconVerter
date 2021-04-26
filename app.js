bc3797dc3c43fd084bf4e1c750b22ee956106fba
Waliston Belles <walistonbelles@bol.com.br>
2021-04-26 16:24:40 -0300

    Adicionado classe para tabela

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var Reader = require('./Reader');
 var Processor = require('./Processor');
+var Table = require('./Table');
 
 var leitor = new Reader();
 
 async function main(){
     var dados = await leitor.Read('./users.csv');
     var dadosProcessados = Processor.Process(dados);
+
+    var usuarios = new Table(dadosProcessados);
+
+    usuarios.rows.push(['Joao Joao', 'Formação PHP', 'PHP', '6']);
+    console.log(usuarios.RowCount);
 }   
 
 main();
